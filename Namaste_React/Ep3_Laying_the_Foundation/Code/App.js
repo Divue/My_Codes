2029a4d6c0b703e663e8fdf829f2886d7dfaf09c
Divue <divue2003@gmail.com>
2024-07-21 01:34:37 +0530

    n

--- Namaste_React/Ep3_Laying_the_Foundation/Code/App.js
+++ Namaste_React/Ep3_Laying_the_Foundation/Code/App.js
@@ -6,6 +6,8 @@ const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(heading);
 
+
 // JSX -> HTML like but not html
 const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>
 console.log(jsxHeading)
+root.render(jsxHeading);
